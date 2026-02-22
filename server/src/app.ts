c210e58a38796e7bdfd9bf2d348670ca3304ad06
Alejandro <10653@holbertonstudents.com>
2026-02-22 16:20:05 -0300

    Update client source

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,8 +14,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use("/api/v1", reservationRouter);
-
 app.use(cors({
     origin: [
         "http://localhost:5173",
@@ -27,18 +25,19 @@ app.use(cors({
 
 app.options('*', cors());
 
-app.get("/", (_req, res) => res.send("HBnB API is running."));
-
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/places", placeRouter);
 app.use("/api/v1/amenities", amenityRouter);
 app.use("/api/v1/places/:placeId/reviews", reviewRouter);
+app.use("/api/v1", reservationRouter);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+const clientBuildPath = path.join(__dirname, "../client/build");
+app.use(express.static(clientBuildPath));
+
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
 export default app;
